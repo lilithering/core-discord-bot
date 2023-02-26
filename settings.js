83157c33fb26b768497dff0b77c61019e0701a9a
lilithering@gmail.com <lilithering@gmail.com>
2023-02-26 15:51:35 -0300

    build 2302.01 version 1.1.0

--- settings.js
+++ settings.js
@@ -8,10 +8,26 @@ const MOD = {
 const BASE = {
     client: {
         token: MOD.secret.client.token,
+        default: {
+            intents: [MOD.discord.GatewayIntentBits.Guilds, MOD.discord.GatewayIntentBits.MessageContent, MOD.discord.GatewayIntentBits.GuildMessages],
+        }
     },
+};
+
+const MAKE = {
+    client: (options = { intents: new MOD.discord.IntentsBitField(BASE.client.default.intents) }) => {
+        const client = new MOD.discord.Client(options);
+        return client;
+    },
+};
+
+const DEFINES = {
+    event: MOD.discord.Events,
 };
 
 module.exports = {
     mod: MOD,
     data: { ...BASE },
+    make: MAKE,
+    ...DEFINES,
 };
