2ce1a7d2f7a0b843c3288f17d1cd9ede7f6cfa55
lilithering@gmail.com <lilithering@gmail.com>
2023-02-26 15:05:30 -0300

    2.0.0

--- settings.js
+++ settings.js
@@ -10,14 +10,15 @@ const IMPORTS = {
         }
         catch (err) {
             const version = '14.7.1';
-            console.error(`Esse núcleo requer o módulo (discord.js) versão ${version} ou superior.\nExecute o comando:\nnpm install discord.js@^${version}`);
-            console.error('Ou adicione: "discord.js": "^14.7.1" em dependencies no seu arquivo package.json.');
+            console.error(`ERRO!\nEsse núcleo requer o módulo (discord.js) versão ${version} ou superior.\n-------\n\nExecute o comando:\nnpm install discord.js@^${version}\n\nOu\n\nAdicione: "discord.js": "^14.7.1"\nem 'dependencies' no seu arquivo package.json, e execute o comando:\nnpm install`);
+            process.exit();
         };
     },
     secret: () => {
         let _filename = 'secret.json';
+        let _path = BUILTIN.path.resolve(_filename);
         try {
-            return require(`./${_filename}`);
+            return require(_path);
         }
         catch (err) {
             console.log(`Criando o arquivo ${_filename} ...`);
@@ -28,7 +29,7 @@ const IMPORTS = {
                 prompt.close();
             });
             prompt.on('close', () => {
-                BUILTIN.filesystem.writeFileSync(_filename, JSON.stringify(data));
+                BUILTIN.filesystem.writeFileSync(_path, JSON.stringify(data));
                 process.exit(0);
             });
         };
