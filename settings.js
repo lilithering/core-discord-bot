41b73b4d37f8a25bde5e5eda63e7da14f091ee9a
lilithering@gmail.com <lilithering@gmail.com>
2023-02-28 17:45:42 -0300

    build 2302.02 version 21.0.1

--- settings.js
+++ settings.js
@@ -170,6 +170,8 @@ const IAEX = {
                     (async () => {
                         const username = interaction.user.username;
                         const docurl = IABX.drive[data[0].sentence];
+                        console.log('docurl>')
+                        console.log(docurl)
                         if (docurl) {
                             const content = await ECX.cloud(docurl);
                             const dataframe = ECX.driver('laboratorio.py', content);
