1ec134bda4b3b0811ce7a3c34a8ea3be1ded1d59
lilithering@gmail.com <lilithering@gmail.com>
2023-02-27 21:44:33 -0300

    build 2302.02 version 15 sub 2

--- settings.js
+++ settings.js
@@ -33,7 +33,7 @@ const IAEX = {
 
 const IAAX = {
     trigger: {
-        labs: [/(traga para|me (diga|conte|fala)|conte me)|qual (é|e|o)/i, (interaction, data) => {
+        labs: [/(traga para|me (diga|conte|fala)|conte me)|qual (é|e|o)|diga me/i, (interaction, data) => {
             // @debug
             console.log(`word> ${data.value}`);
             // traga para mim os resultados do laboratório globo-frontend
