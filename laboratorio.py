758854b606e2f8b360f033a0e800496c35b2939e
lilithering@gmail.com <lilithering@gmail.com>
2023-02-28 15:14:23 -0300

    build 2302.02 version 16 sub 1.7

--- laboratorio.py
+++ laboratorio.py
@@ -2,6 +2,4 @@ import pandas
 import sys
 
 dataframe = pandas.read_csv(sys.stdin, encoding='utf-8')
-dataframe.set_index(dataframe.columns[0])
-dataframe.isna('C')
-dataframe.to_json(sys.stdout, force_ascii=False)
+dataframe.set_index(dataframe.columns[0]).fillna('C').to_json(sys.stdout, force_ascii=False)
